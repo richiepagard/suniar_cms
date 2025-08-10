b2698139807ded1823b7e1dbc187868da5bbca9b
Mohsen <mohsen.h.rad.82@gmail.com>
2025-08-10 20:41:39 +0330

    Define and Customize category admin page

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from category.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ Customize admin page for category model. """
+    ordering = ['id']
+    empty_value_display = '-empty-'
+    list_display = ['name', 'description', 'is_active']
+    fieldsets = [
+        (
+            None, {'fields': ('name',)}
+        ),
+        (
+            'جزئیات', {'fields': ('description', 'is_active')}
+        )
+    ]
+
+    add_fieldset = (
+        (None, {
+            'class': ('wide',),
+            'fields': ('name', 'description', 'is_active')
+        }),
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
