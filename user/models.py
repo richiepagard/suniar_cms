d7ec2814f6361469d0fbbf5ac1d3907a3badbefa
richie <richiepagard@gmail.com>
2025-08-12 09:46:52 +0330

    feat: Makes user model and manager more effeicent.
    - Implement user custom manager and separate it to the managers file.
    
    - Refactor custom user model to using phone number field and add some importatn method and fields.
    
    - Improve doc-string for user managers and user models.

--- user/models.py
+++ user/models.py
@@ -4,70 +4,82 @@ from django.contrib.auth.models import (
     BaseUserManager
 
 )
-
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-
-class UserManager(BaseUserManager):
-    """ Manage for Users."""
-
-    def create_user(self, email, password, **extra_fields):
-        """ Create,Save and return a user. """
-        # if not email:
-        #     raise ValueError('Email field is necessary')
-        # email = self.normalize_email(email)
-        # user = self.model(email=email, **extra_fields)
-        # user.set_password(password)
-        # user.save(using=self._db)
-        #
-        # return user
-        if not email:
-            raise ValueError('Email field is necessary')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self.db)
-
-        return user
-
-    def create_superuser(self, email, password):
-        """ Create and save a superuser. """
-        user = self.create_user(email, password)
-        user.is_active = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self.db)
-
-        return user
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """ User in the system. """
+    """
+    Represents users in the project, customize default user model.
 
+    Arguments:
+        phone_numbe (str): Unique phone number to authenticate the user.
+        username (str): Unique username to identify and display name of user.
+        email (str): Unique email address to authenticate with email for account.
+        first_name (str): User's first name.
+        last_name (str): User's last name.
+        is_active (bool): The activation status of user.
+        is_staff (bool): The staff mode of a user, same as admin users.
+        role (str): User's specific role.
+    """
     class Role(models.TextChoices):
         user = "user", "Normal User"
         super_user = "super_user", "Super User"
         programmer = "programmer", "Programmer"
         ceo = "ceo", "CEO"
 
-    email = models.EmailField(max_length=255, unique=True, blank=False, null=False)
-    first_name = models.CharField(max_length=255, blank=True)
-    last_name = models.CharField(max_length=255, blank=True)
-    mobile = models.CharField(max_length=11, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    role = models.CharField(max_length=12, choices=Role.choices, default=Role.user)
-
-    # role = models.CharField(choices=
-    # [
-    #     ('user', 'Normal User'),
-    #     ('super_user', 'Super User'),
-    #     ('programmer', 'Programmer'),
-    #     ('ceo', 'CEO')
-    # ],
-    #     default=models.Choices
-    # )
+    phone_number = PhoneNumberField(
+        region="IR",
+        unique=True,
+        verbose_name=_("Phone number")
+    )
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        verbose_name=_("Username")
+    )
+    email = models.EmailField(
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name=_("Email")
+    )
+    first_name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name=_("First name")
+    )
+    last_name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name=_("Last name")
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("Activation status")
+    )
+    is_staff = models.BooleanField(
+        default=False,
+        verbose_name=_("Staff situation")
+    )
+    role = models.CharField(
+        max_length=12,
+        choices=Role.choices,
+        default=Role.user,
+        verbose_name=_("Role")
+    )
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "phone_number"
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self) -> str:
+        return f"{self.phone_number} - {self.username}"
